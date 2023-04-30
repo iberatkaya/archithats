e205032cc639852dbdd210dfe61c3465293ca1af
iberatkaya <beratkaya1998@gmail.com>
2023-04-29 21:14:44 -0500

    Added GH Pages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,12 +5,12 @@ const config: GatsbyConfig = {
     title: `Archithats`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: "/archithats",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-google-gtag",
     "gatsby-plugin-image",
     {
       resolve: "gatsby-plugin-manifest",
