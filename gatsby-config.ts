894de9f1b543fc0d59f3b80f61fdc5b60dc7180d
iberatkaya <beratkaya1998@gmail.com>
2024-04-05 20:55:01 -0500

    Fixed domain

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,9 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Archithats`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://archithats.com`,
   },
-  pathPrefix: "/archithats",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
