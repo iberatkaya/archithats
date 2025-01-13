c42efce174c696ed79b3160f31dcd75f9e7074c5
Ibrahim Kaya <ibraberatkaya@gmail.com>
2025-01-12 20:35:42 -0600

    Added animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import "../styles/global.css";
-import PDF from "../assets/pdfs/demo.pdf";
 import Logo from "../images/archithats.jpg";
 
 const pageStyles = {
@@ -14,19 +13,10 @@ const IndexPage: React.FC<PageProps> = () => {
       <div>
         <div className="bg-black w-full align-middle justify-center">
           <img
-            className="h-96 sm:h-[100vh] w-72 sm:w-96 object-contain mx-auto"
+            className="h-96 sm:h-[100vh] w-72 sm:w-96 object-contain mx-auto animate-pulse"
             src={Logo}
           />
         </div>
-        <p className="px-6 text-white text-xl sm:text-3xl mb-2 sm:mb-6 sm:px-64 sm:ml-4">
-          April 2023
-        </p>
-        <iframe
-          src={PDF}
-          allowFullScreen
-          frameBorder="0"
-          className="w-full px-4 sm:px-64 h-[90vh]"
-        />
       </div>
     </main>
   );
