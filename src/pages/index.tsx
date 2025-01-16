7696d80c3f807fda048e519f18946f731e9593d4
Ibrahim Kaya <ibraberatkaya@gmail.com>
2025-01-15 20:58:50 -0600

    Made background color white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import "../styles/global.css";
-import Logo from "../images/archithats.jpg";
+import Logo from "../images/archithats-logo.jpg";
 
 const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  backgroundColor: "white", // Add this line
 };
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles} className="min-h-screen flex items-center justify-center">
+    <main
+      style={pageStyles}
+      className="min-h-screen flex items-center justify-center bg-white" // Add bg-white class
+    >
       <div className="w-full">
         <img
           className="h-96 sm:h-[100vh] w-72 sm:w-96 object-contain mx-auto animate-pulse"
